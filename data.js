ac2df7197448ecdeb9c449a44754eef2253627a2
Bruno Stefano de Souza <40743118+vonkoln@users.noreply.github.com>
2024-01-24 20:29:23 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,26 +1,50 @@
 export default  [  
   {
-    title:"3 - Bach",
-    artist: "",
+    title:"1 - Christmas Oratorio",
+    artist: "Johann Sebastian Bach",
+    cover: "bach.jpeg",
+    file: "bach1.mp3",
+  }, 
+  {
+    title:"2 - Christmas Oratorio",
+    artist: "Johann Sebastian Bach",
+    cover: "bach.jpeg",
+    file: "bach2.mp3",
+  }, 
+  {
+    title:"3 - Christmas Oratorio",
+    artist: "Johann Sebastian Bach",
     cover: "bach.jpeg",
     file: "bach3.mp3",
   },  
   {
-    title:"4 - Bach",
-    artist: "",
+    title:"4 - Christmas Oratorio",
+    artist: "Johann Sebastian Bach",
     cover: "bach.jpeg",
     file: "bach4.mp3",
   },  
   {
-    title:"5 - Bach",
-    artist: "",
+    title:"5 - Christmas Oratorio",
+    artist: "Johann Sebastian Bach",
     cover: "bach.jpeg",
     file: "bach5.mp3",
   },  
   {
-    title:"6 - Bach",
-    artist: "Bach",
+    title:"6 - Christmas Oratorio",
+    artist: "Johann Sebastian Bach",
     cover: "bach.jpeg",
     file: "bach6.mp3",
+  },
+  {
+    title:"7- Christmas Oratorio",
+    artist: "Johann Sebastian Bach",
+    cover: "bach.jpeg",
+    file: "bach7.mp3",
+  }, 
+  {
+    title:"8 - Christmas Oratorio",
+    artist: "Johann Sebastian Bach",
+    cover: "bach.jpeg",
+    file: "bach8.mp3",
   }
  ];
